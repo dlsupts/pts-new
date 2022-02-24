e131b9af350dfc76889123305c064aa0a01c8b1d
Jared Sy <jaredblase@gmail.com>
2022-02-24 14:32:12 +0800

    add: windicss and zustand

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const WindiCssWebpackPlugin = require('windicss-webpack-plugin')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack(config) {
+    config.plugins.push(new WindiCssWebpackPlugin())
+    return config
+  },
 }
 
 module.exports = nextConfig
