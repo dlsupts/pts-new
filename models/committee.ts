e6641ccabe0214f75e63661452ece1c990aa5caa
Jared Sy <jaredblase@gmail.com>
2022-02-26 03:07:58 +0800

    add: committee model

---
+++ models/committee.ts
@@ -0,0 +1,27 @@
+import { Schema, models, model, Model } from 'mongoose'
+
+export interface IOfficer {
+	account: Schema.Types.ObjectId | string;
+	position: string;
+	image: string;
+}
+
+export interface ICommittee {
+	name: string;
+	officers: IOfficer[];
+}
+
+const committeeSchema = new Schema<ICommittee>({
+	name: { type: String, required: true },
+	officers: [{
+		account: {
+			type: Schema.Types.ObjectId,
+			ref: 'Account',
+			required: true,
+		},
+		position: { type: String, required: true },
+		image: { type: String, required: true },
+	}]
+})
+
+export default models.Committee as Model<ICommittee> || model<ICommittee>('Committee', committeeSchema, 'committees')
