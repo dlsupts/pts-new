ee994af7d71af6b4cbc45ed6a8f4c7a55b9976d9
Jared Sy <jaredblase@gmail.com>
2022-03-09 01:59:28 +0800

    refactor: useUser to use SRWImmutability

--- lib/useUser.ts
+++ lib/useUser.ts
@@ -1,11 +1,11 @@
-import useSWR from 'swr'
+import useSWRImmutable from 'swr/immutable'
 import app from './axios-config'
 import { IUser } from '../models/user'
 
 const fetcher = (url: string) => app.get<IUser>(url)
 
 export default function useUser() {
-	const { data, error, mutate } = useSWR('/api/me', fetcher)
+	const { data, error, mutate } = useSWRImmutable('/api/me', fetcher)
 	return {
 		user: data?.data,
 		isLoading: !data && !error,
