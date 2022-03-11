562ec9dd1cc07ea308fa6033c74d13753aa77730
Jared Sy <jaredblase@gmail.com>
2022-03-12 00:24:30 +0800

    refactor: extract subject list and modal to add subjects

---
+++ components/subject-list/list.tsx
@@ -0,0 +1,33 @@
+import { Dispatch, FC, SetStateAction } from 'react'
+
+interface SubjectListProps {
+	subjects: string[][]
+	setSubjects: Dispatch<SetStateAction<string[][]>>
+}
+
+const SubjectList: FC<SubjectListProps> = ({ subjects, setSubjects }) => {
+	if (!subjects.length) {
+		return (
+			<p className="text-center text-gray-500">No Subjects Added</p>
+		)
+	}
+	
+	return (
+		<>
+			{subjects.map(t => (
+				<div className="py-2 border-b flex justify-between items-stretch" key={t[0]}>
+					<div>
+						<p className="font-medium">{t[0]}</p>
+						<p className="text-gray-500 text-sm">Specific topics: {t[1] || 'None'}</p>
+					</div>
+					<div className="btn mr-8 grid place-items-center text-gray-500 hover:text-gray-600 w-8"
+						onClick={() => setSubjects(subjects.filter(s => s[0] !== t[0]))}>
+						<i className="fa-solid fa-trash fa-lg"></i>
+					</div>
+				</div>
+			))}
+		</>
+	)
+}
+
+export default SubjectList
---
+++ components/subject-list/modal.tsx
@@ -0,0 +1,60 @@
+import { Dialog } from '@headlessui/react'
+import { Dispatch, FC, SetStateAction, useRef } from 'react'
+import Modal from '../modal'
+import { filterSubjects, binInsert } from '../../lib/utils'
+
+interface AddSubjectModalProps {
+	isOpen: boolean
+	setIsOpen: Dispatch<SetStateAction<boolean>>
+	options: string[]
+	selected: string[][]
+	setSelected: Dispatch<SetStateAction<string[][]>>
+}
+
+const AddSubjectModal: FC<AddSubjectModalProps> = ({ isOpen, setIsOpen, options, selected, setSelected }) => {
+	const closeModal = () => setIsOpen(false)
+	const newSubject = useRef<HTMLSelectElement>(null)
+	const newTopics = useRef<HTMLTextAreaElement>(null)
+
+	const addSubject = () => {
+		const subject = newSubject?.current?.value || ''
+		const topic = newTopics?.current?.value || ''
+
+		// verify if value is in list of subjects
+		if (options.find(s => s == subject)) {
+			const toSave = [subject.split(': ')[0], topic]
+			binInsert(selected, toSave)
+			setSelected(selected)
+		}
+
+		closeModal()
+	}
+
+	return (
+		<Modal isOpen={isOpen} close={closeModal}>
+			<div className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+				<div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+					<div className="sm:flex sm:items-start">
+						<div className="mt-3 text-center sm:mt-0 sm:text-left">
+							<Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">Add Subject</Dialog.Title>
+							<div className="mt-4">
+								<label htmlFor="new-subject">New Subject</label>
+								<select ref={newSubject} id="new-subject">
+									{filterSubjects(options, selected).map(o => <option key={o} value={o}>{o}</option>)}
+								</select>
+								<label htmlFor="specific-topics" className="mb-1 mt-4">Specific Topics</label>
+								<textarea ref={newTopics} id="specific-topics" rows={3} className="shadow-sm w-full focus:ring-blue-500 focus:border-blue-500 sm:text-sm border border-gray-300 rounded-md"></textarea>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="bg-gray-50 px-4 py-3 sm:px-6 flex flex-row-reverse">
+					<input value="Add" type="submit" onClick={addSubject} className="blue btn px-4 py-2 shadow-sm sm:w-auto sm:text-sm rounded-md font-medium" />
+					<button type="button" onClick={closeModal} className="white btn px-4 py-2 rounded-md shadow-sm font-medium sm:mt-0 sm:w-auto sm:text-sm mx-3" > Cancel </button>
+				</div>
+			</div>
+		</Modal>
+	)
+}
+
+export default AddSubjectModal
