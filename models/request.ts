b45109a1f718f22cd3b23aa0cdf6b368842565c5
Jared Sy <jaredblase@gmail.com>
2022-03-12 18:34:00 +0800

    add: request model

---
+++ models/request.ts
@@ -0,0 +1,31 @@
+import { Schema, models, model, Model, Document } from 'mongoose'
+import { ITutee } from './tutee'
+import { IUser } from './user'
+
+export interface IRequest {
+	timestamp: Date
+	ayterm: string
+	duration: 'Whole Term' | 'One Session'
+	tutorialType: string
+	preferred: Schema.Types.ObjectId | IUser | null
+	tutee: Schema.Types.ObjectId | ITutee
+	seen: boolean
+}
+
+const requestSchema = new Schema<IRequest>({
+	timestamp: { type: Date, default: Date.now },
+	ayterm: String,
+	duration: String,
+	tutorialType: String,
+	preferred: {
+		type: Schema.Types.ObjectId,
+		ref: 'Account'
+	},
+	tutee: {
+		type: Schema.Types.ObjectId,
+		ref: 'Tutee'
+	},
+	seen: Boolean
+})
+
+export default models.Request as Model<IRequest & Document> || model<IRequest>('Request', requestSchema, 'requests')
