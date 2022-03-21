a2c9e3efe4729f03b99c75b1ec5837ec38d9168e
Jared Sy <jaredblase@gmail.com>
2022-03-21 10:35:45 +0800

    fix: ui scrollable table

--- components/table/table.tsx
+++ components/table/table.tsx
@@ -28,34 +28,36 @@ const Table = <T extends object>({ columns, data }: tableprops<T>) => {
 	)
 
 	return (
-		<div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+		<div className="py-2 align-middle inline-block w-full sm:px-6 lg:px-8 overflow-x-auto">
 			<GlobalFilter
 				globalFilter={state.globalFilter}
 				setGlobalFilter={setGlobalFilter}
 			/>
-			<table {...getTableProps()} className="w-full divide-y divide-gray-200 table-fixed rounded-lg shadow-md overflow-hidden">
-				<thead className="bg-gray-100">
-					{headerGroups.map(headerGroup => (
-						<tr className="text-gray-600" {...headerGroup.getHeaderGroupProps()}>
-							{headerGroup.headers.map(column => (
-								<th scope="col" className="px-6 py-4 text-left text-xs font-bold uppercase tracking-wider" {...column.getHeaderProps()}>{column.render('Header')}</th>
-							))}
-						</tr>
-					))}
-				</thead>
-				<tbody {...getTableBodyProps()} className="bg-white divide-y divide-gray-200">
-					{rows.map((row, i) => {
-						prepareRow(row)
-						return (
-							<tr {...row.getRowProps()} className={cn({ 'bg-gray-50': i % 2 }, 'text-gray-600')}>
-								{row.cells.map(cell => {
-									return <td {...cell.getCellProps()} className="px-6 py-3 whitespace-nowrap text-sm">{cell.render('Cell')}</td>
-								})}
+			<div className="w-full overflow-x-auto">
+				<table {...getTableProps()} className="min-w-full divide-y divide-gray-200 table-fixed rounded-lg shadow-md overflow-hidden">
+					<thead className="bg-gray-100">
+						{headerGroups.map(headerGroup => (
+							<tr className="text-gray-600" {...headerGroup.getHeaderGroupProps()}>
+								{headerGroup.headers.map(column => (
+									<th scope="col" className="px-6 py-4 text-left text-xs font-bold uppercase tracking-wider" {...column.getHeaderProps()}>{column.render('Header')}</th>
+								))}
 							</tr>
-						)
-					})}
-				</tbody>
-			</table>
+						))}
+					</thead>
+					<tbody {...getTableBodyProps()} className="bg-white divide-y divide-gray-200">
+						{rows.map((row, i) => {
+							prepareRow(row)
+							return (
+								<tr {...row.getRowProps()} className={cn({ 'bg-gray-50': i % 2 }, 'text-gray-600')}>
+									{row.cells.map(cell => {
+										return <td {...cell.getCellProps()} className="px-6 py-3 whitespace-nowrap text-sm">{cell.render('Cell')}</td>
+									})}
+								</tr>
+							)
+						})}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	)
 }
