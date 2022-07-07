330eef9b1f1c08b0fa72cacd071515fe9851c503
Jared Sy <jaredblase@gmail.com>
2022-07-07 20:35:09 +0800

    add: additional checks before creating request object

--- pages/api/applications.ts
+++ pages/api/applications.ts
@@ -1,24 +1,48 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
 import dbConnect from '../../lib/db'
 import Application from '../../models/application'
+import User from '../../models/user'
 
-const applyHandler = async (req: NextApiRequest, res: NextApiResponse<boolean>) => {
+const applyHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 	try {
 		await dbConnect()
 
 		switch (req.method) {
-			case "POST": {
+			case 'GET': {
+				// GET operation only for admin
+				const session = await getSession({ req })
+				if (session?.user.type != 'ADMIN') return res.status(403)
+
+				const applicants = await Application.find()
+				res.json(applicants)
+				break
+			}
+
+			case 'POST': {
+				// check if duplicate entry
+				const values = await Promise.all([
+					Application.findOne({ $or: [{ email: req.body.email }, { idNumber: req.body.idNumber }] }, '_id'),
+					User.findOne({ $or: [{ email: req.body.email }, { idNumber: req.body.idNumber }] }, '_id')
+				])
+				
+				if (values[0]) {
+					return res.status(406).send('You have already sent an application!')
+				} else if (values[1]) {
+					return res.status(406).send('You are already a tutor!')
+				}
 				await Application.create(req.body)
+				
 				break
 			}
 
 			default:
-				res.setHeader('Allow', ['POST'])
+				res.setHeader('Allow', ['GET', 'POST'])
 				res.status(405).end(`Method ${req.method} Not Allowed`)
 		}
 	} catch (err) {
 		console.log(err)
-		res.status(500)
+		res.status(500).send('A server-side error has occured. Please try again later.')
 	} finally {
 		res.end()
 	}
