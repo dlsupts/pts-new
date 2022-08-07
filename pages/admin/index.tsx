58ee8fa5d6b18b1d5fbf059071869654240da841
Jared Sy <jaredblase@gmail.com>
2022-08-07 19:10:37 +0800

    fix: dashboard styles

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,6 +1,6 @@
 import AdminLayout from '@components/admin-layout'
 import { NextPage } from 'next'
-import { PieChart, Pie, Legend, Cell, Tooltip, Label } from 'recharts'
+import { PieChart, Pie, Legend, Cell, Tooltip, Label, ResponsiveContainer } from 'recharts'
 import styles from '@styles/Dashboard.module.css'
 import { useRetriever } from '@lib/useRetriever'
 import Head from 'next/head'
@@ -78,32 +78,36 @@ const AdminPage: NextPage = () => {
 					<h1>Tutee Demographic ({totalTutees})</h1>
 					<div className={styles['graphs-container']}>
 						<div>
-							<PieChart {...PIE_CHART_CONFIG}>
-								{legend}
-								{tooltip}
-								<Pie data={data[2]} {...PIE_CONFIG}>
-									{data[2].map((entry, index) => (
-										<Cell key={entry._id} name={entry._id} fill={scale.colors(data[2].length)[index]} />
-									))}
-									<Label className={styles['middle-label']} position="center">
-										ID Number
-									</Label>
-								</Pie>
-							</PieChart>
+							<ResponsiveContainer>
+								<PieChart {...PIE_CHART_CONFIG}>
+									{legend}
+									{tooltip}
+									<Pie data={data[2]} {...PIE_CONFIG}>
+										{data[2].map((entry, index) => (
+											<Cell key={entry._id} name={entry._id} fill={scale.colors(data[2].length)[index]} />
+										))}
+										<Label className={styles['middle-label']} position="center">
+											ID Number
+										</Label>
+									</Pie>
+								</PieChart>
+							</ResponsiveContainer>
 						</div>
 						<div>
-							<PieChart {...PIE_CHART_CONFIG}>
-								{legend}
-								{tooltip}
-								<Pie data={data[1]} {...PIE_CONFIG}>
-									{data[1].map((entry, index) => (
-										<Cell key={entry._id} name={entry._id} fill={scale.colors(data[1].length)[index]} />
-									))}
-									<Label className={styles['middle-label']} position="center">
-										College
-									</Label>
-								</Pie>
-							</PieChart>
+							<ResponsiveContainer>
+								<PieChart {...PIE_CHART_CONFIG}>
+									{legend}
+									{tooltip}
+									<Pie data={data[1]} {...PIE_CONFIG}>
+										{data[1].map((entry, index) => (
+											<Cell key={entry._id} name={entry._id} fill={scale.colors(data[1].length)[index]} />
+										))}
+										<Label className={styles['middle-label']} position="center">
+											College
+										</Label>
+									</Pie>
+								</PieChart>
+							</ResponsiveContainer>
 						</div>
 					</div>
 				</section>
@@ -112,37 +116,41 @@ const AdminPage: NextPage = () => {
 					<h1>Tutor Demographic ({totalTutors})</h1>
 					<div className={styles['graphs-container']}>
 						<div>
-							<PieChart {...PIE_CHART_CONFIG}>
-								{legend}
-								{tooltip}
-								<Pie data={data[3]} {...PIE_CONFIG}>
-									{data[3].map((entry, index) => (
-										<Cell key={entry._id} name={entry._id} fill={scale.colors(data[3].length)[index]} />
-									))}
-									<Label className={styles['middle-label']} position="center">
-										ID Number
-									</Label>
-								</Pie>
-							</PieChart>
+							<ResponsiveContainer>
+								<PieChart {...PIE_CHART_CONFIG}>
+									{legend}
+									{tooltip}
+									<Pie data={data[3]} {...PIE_CONFIG}>
+										{data[3].map((entry, index) => (
+											<Cell key={entry._id} name={entry._id} fill={scale.colors(data[3].length)[index]} />
+										))}
+										<Label className={styles['middle-label']} position="center">
+											ID Number
+										</Label>
+									</Pie>
+								</PieChart>
+							</ResponsiveContainer>
 						</div>
 						<div>
-							<PieChart {...PIE_CHART_CONFIG}>
-								{legend}
-								{tooltip}
-								<Pie data={data[4]} {...PIE_CONFIG}>
-									{data[4].map((entry, index) => (
-										<Cell key={entry._id} name={entry._id} fill={scale.colors(data[4].length)[index]} />
-									))}
-									<Label className={styles['middle-label']} position="center">
-										Program
-									</Label>
-								</Pie>
-							</PieChart>
+							<ResponsiveContainer>
+								<PieChart {...PIE_CHART_CONFIG}>
+									{legend}
+									{tooltip}
+									<Pie data={data[4]} {...PIE_CONFIG}>
+										{data[4].map((entry, index) => (
+											<Cell key={entry._id} name={entry._id} fill={scale.colors(data[4].length)[index]} />
+										))}
+										<Label className={styles['middle-label']} position="center">
+											Program
+										</Label>
+									</Pie>
+								</PieChart>
+							</ResponsiveContainer>
 						</div>
 					</div>
 				</section>
-			</div>
-		</AdminLayout>
+			</div >
+		</AdminLayout >
 	)
 }
 
