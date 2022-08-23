47884ed43e59f28e3dcb5e205bf6c6599fa34698
Jared Sy <jaredblase@gmail.com>
2022-08-23 16:05:16 +0800

    add: loading button copmonent

---
+++ components/loading-button.tsx
@@ -0,0 +1,21 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react'
+import cn from 'classnames'
+
+type LoadingButtonProps = {
+	isLoading: boolean
+	children: ReactNode
+} & ButtonHTMLAttributes<HTMLButtonElement>
+
+const LoadingButton = ({ isLoading, children, className, disabled, ...props }: LoadingButtonProps) => {
+
+	return (
+		<button className={cn(className, 'relative', { '!text-transparent': isLoading })} disabled={disabled || isLoading} {...props}>
+			{isLoading &&
+				<div className="absolute h-full w-full left-1/2 top-1/2 grid place-items-center -translate-x-1/2 -translate-y-1/2 loading" />
+			}
+			{children}
+		</button>
+	)
+}
+
+export default LoadingButton
