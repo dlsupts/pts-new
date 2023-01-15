009c8a4400c81cfe8fec91e09f5dd243cefba150
Jared <jaredblase@gmail.com>
2023-01-15 16:16:45 +0800

    add: policy page

--- components/header.tsx
+++ components/header.tsx
@@ -26,6 +26,7 @@ const navItems: NavItemProp[] = [
 	{ path: '/about', text: 'About' },
 	{ path: '/request', text: 'Request' },
 	{ path: '/apply', text: 'Apply' },
+	{ path: '/policies', text: 'Policies' }
 ]
 
 const MobNavItem: FC<MobNavItemProp> = ({ text, path, isHome, onClick }) => {
@@ -86,24 +87,24 @@ const Header: FC = () => {
 					<>
 						<div className="container mx-auto px-2 sm:px-6 lg:px-8 h-full">
 							<div className="relative flex items-center justify-between h-full">
-								<div className="absolute inset-y-0 left-0 flex items-center md:hidden">
+								<div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
 									<Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
 										<span className="sr-only">Open main menu</span>
 										{open ? <XIcon className="block h-6 w-6" aria-hidden="true" /> : <MenuIcon className="block h-6 w-6" aria-hidden="true" />}
 									</Disclosure.Button>
 								</div>
-								<div className="flex-1 flex items-center h-full justify-center md:items-stretch md:justify-start">
+								<div className="flex-1 flex items-center h-full justify-center lg:items-stretch lg:justify-start">
 									<Link href="/" passHref className="flex-shrink-0 flex items-center cursor-pointer mr-6">
 										<Image className="block w-auto" src={Logo} alt="PTS Logo" width={96} height={50} />
 									</Link>
-									<div className="hidden md:block md:ml-6">
+									<div className="hidden lg:block lg:ml-6">
 										<div className="flex space-x-4 items-center h-full">
 											{navItems.map(nav => <NavItem key={nav.text} text={nav.text} path={nav.path} isHome={nav.isHome} />)}
 											{isAuthenticated && <NavItem text={'My Profile'} path={'/me'} />}
 										</div>
 									</div>
 								</div>
-								<div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
+								<div className="absolute inset-y-0 right-0 flex items-center pr-2 lg:static lg:inset-auto lg:ml-6 lg:pr-0">
 									<LoadingButton
 										className="blue btn font-medium px-4 py-2 rounded-md"
 										onClick={handleAuthClick}
@@ -124,7 +125,7 @@ const Header: FC = () => {
 							leaveTo="transform scale-95 opacity-0"
 						>
 
-							<Disclosure.Panel className="md:hidden relative bg-white">
+							<Disclosure.Panel className="lg:hidden relative bg-white">
 								<div className="px-2 pt-2 pb-3 space-y-1">
 									{navItems.map(nav => <MobNavItem key={nav.text} text={nav.text} path={nav.path} isHome={nav.isHome} onClick={close} />)}
 									{isAuthenticated && <MobNavItem text={'My Profile'} path={'/me'} onClick={close} />}
