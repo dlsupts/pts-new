e62b9ca883b0471bfe50fffee5b6a63a1d8470ff
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2023-10-02 01:31:48 +0800

    update acceptance letter

--- components/mail/acceptance.tsx
+++ components/mail/acceptance.tsx
@@ -4,11 +4,11 @@ export default function AcceptanceEmail(applicant: IUserInfo) {
 	return (
 		<>
 			<p>Good day, {applicant.firstName}</p>
-			<p>We are glad to inform you that you have <b className='underline'>passed</b> the application interview and are officially a tutor of the Peer Tutors Society!</p>
-			<p>With this, must attend two <b>Tutoring 101</b>.</p>
+			<p>We are glad to inform you that you have <b>passed</b> the application interview and are officially a tutor of the Peer Tutors Society!</p>
+			<p>With this, all new members must attend <b>Tutoring 101</b>.</p>
 			<ul>
 				<li>
-					The <b>Tutoring 101</b> will be held virtually on <b>October 7, 2023</b>, from <b>4 pm</b> to <b>5 pm</b> in <b>Zoom</b>. More details will be sent in the coming days.
+					<b>Tutoring 101</b> will be held virtually on <b>October 7, 2023</b>, from <b>4 pm</b> to <b>5 pm</b> in <b>Zoom</b>. More details will be sent in the coming days.
 				</li>
 			</ul>
 			<p>
