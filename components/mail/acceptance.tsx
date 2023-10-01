f41af2ae234d0b5dbf3941100252c35948b5a86d
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2023-10-02 01:19:29 +0800

    update acceptance letter

--- components/mail/acceptance.tsx
+++ components/mail/acceptance.tsx
@@ -4,20 +4,15 @@ export default function AcceptanceEmail(applicant: IUserInfo) {
 	return (
 		<>
 			<p>Good day, {applicant.firstName}</p>
-			<p>We are glad to inform you that you have passed the application interview and are officially a tutor of the Peer Tutors Society!</p>
-			<p>With this, all new members are required to attend two (2) upcoming events, the General Assembly and Tutoring 101.</p>
+			<p>We are glad to inform you that you have <b className='underline'>passed</b> the application interview and are officially a tutor of the Peer Tutors Society!</p>
+			<p>With this, must attend two <b>Tutoring 101</b>.</p>
 			<ul>
 				<li>
-					The <b>General Assembly</b> will be held on <b>June 7, 2023</b>, from <b>4 pm to 6 pm in Zoom</b>. Kindly answer
-					the <a href="https://forms.gle/DJY954ueh5qM3BBa8">New Tutors Form</a> within the day.
-				</li>
-				<li>
-					<b>Tutoring 101</b> will be held virtually on <b>June 10, 2023, from 4 pm to 5 pm in Zoom</b>.
-					More details will be sent in the coming days.
+					The <b>Tutoring 101</b> will be held virtually on <b>October 7, 2023</b>, from <b>4 pm</b> to <b>5 pm</b> in <b>Zoom</b>. More details will be sent in the coming days.
 				</li>
 			</ul>
 			<p>
-				Please reply with your active Facebook account link as soon as you can so that we may add you to our social media groups.
+				Please reply with your active Facebook account link as soon as possible so that we may add you to our social media groups.
 				Thank you and we are looking forward to having you here with us in PTS!
 			</p>
 		</>
