4ed636eda05e3f715110142460b8bef00d30f899
Revkxd <79917173+Revkxd@users.noreply.github.com>
2024-05-27 10:17:35 +0800

    Change acceptance email

--- components/mail/acceptance.tsx
+++ components/mail/acceptance.tsx
@@ -5,16 +5,21 @@ export default function AcceptanceEmail(applicant: IUserInfo) {
 		<>
 			<p>Good day, {applicant.firstName}</p>
 			<p>We are glad to inform you that you have <b>passed</b> the application interview and are officially a tutor of the Peer Tutors Society!</p>
-			<p>With this, all new members must attend <b>Tutoring 101</b>.</p>
+			<p>With this, all new members are <b>required</b> to attend the <b>Tutoring 101</b> session and the <b>General Assembly</b>.</p>
 			<ul>
 				<li>
-					<b>Tutoring 101</b> will be held virtually on <b>October 7, 2023</b>, from <b>4 pm</b> to <b>5 pm</b> in <b>Zoom</b>. More details will be sent in the coming days.
+					<b>Tutoring 101</b> will be held virtually on <b>May 29, 2024</b>, from <b>7 pm</b> to <b>8 pm</b> in <b>Zoom</b>. More details will be sent in the coming days.
+				</li>
+				<li>
+					The <b>General Assembly</b> will be held on <b>June 1, 2024</b> from <b>2 pm</b> to <b>4 pm</b> in <b>AG1703</b>.
 				</li>
 			</ul>
 			<p>
-				Please check your message requests on Facebook Messenger for an invitation to our PTS Tutors group chat.
-				Thank you and we are looking forward to having you here with us in PTS!
+				Please reply with your active Facebook account link as soon as possible so that we may add you to our social media groups. Thank you and we are looking forward to having you here with us in PTS!
 			</p>
+			<p><i>
+				Share. Inspire. Grow
+			</i></p>
 		</>
 	)
 }
